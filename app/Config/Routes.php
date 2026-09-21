1296b9792c1a20771aca1f4975869bf18a88ea5b
Gibble-Nibble <wcdejose@fit.edu.ph>
2026-09-21 12:34:54 +0800

    Update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,7 +14,7 @@ $routes->get('/M1/users', 'Users::index');
 // Module 2 routes
 $routes->get('/M2/index', 'Pages::module2_index');
 $routes->get('/M2/about', 'Pages::module2_about');
-$routes->get('/M2/customers', 'Customers::index');
-$routes->get('/M2/users', 'Users::index');
+$routes->get('/M2/customers', 'Customers::module2_index');
+$routes->get('/M2/users', 'Users::module2_index');
 
 // Module 3 routes
